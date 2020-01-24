5f48db17fb4381ba15c0598aa68272838a96d2a4
softchris <noringc@gmail.com>
2020-01-24 00:21:14 +0000

    adding start script and new route

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const products = [{
+  id: 1,
+  name: 'Star Wars'
+}];
+
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/products', (req, res) => res.json(products))
+
 app.listen(port, () => console.log(`Example app listening on ${port} port!`))
