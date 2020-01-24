37bbee8b0ca5d43a779b48499464eaa03d47173d
softchris <noringc@gmail.com>
2020-01-24 00:25:17 +0000

    adding new entry

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const products = [{
-  id: 1,
-  name: 'Star Wars'
-}];
+const products = [
+  {
+    id: 1,
+    name: "Star Wars"
+  },
+  {
+    id: 2,
+    name: "Empire Strikes back"
+  }
+];
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/products', (req, res) => res.json(products))
